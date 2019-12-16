ee5424bed626cca6a4be40375878aa7d5fba0b2d
qeqar <mark.eisenblaetter@gmail.com>
2019-12-16 17:15:05 +0100

    much easier intcomp code

--- 2019/intComp.py
+++ 2019/intComp.py
@@ -3,39 +3,7 @@ import threading
 import queue
 
 
-def parameter_mode(opcode, param):
-    if param == 1:
-        p = -3
-    elif param == 2:
-        p = -4
-    elif param == 3:
-        p = -5
-    else:
-        print("wrong parameter for mode")
-        exit(1)
-    return int(str(opcode)[p]) if len(str(opcode)) > param + 1 else 0
 
-
-def get_value(codes, mode, i, param, rbase):
-    if mode == 0:  # position mode
-        index = codes[i + param]
-    elif mode == 1:  # immediate mode
-        index = i + param
-    elif mode == 2:  # relative mode
-        index = codes[i + param] + rbase
-    else:
-        print("mode " + str(mode) + " not supported")
-        exit(1)
-    if index >= len(codes):
-        return 0
-    return codes[index]
-
-
-def add_value(codes, position, value):
-    if position >= len(codes):
-        for _ in range(len(codes), position + 1):
-            codes.append(0)
-    codes[position] = value
 
 
 class Comp(threading.Thread):
@@ -49,51 +17,82 @@ class Comp(threading.Thread):
         self.input_queue = input_queue
         self.output_queue = output_queue
 
+    def parameter_mode(self, param):
+        if param == 1:
+            p = -3
+        elif param == 2:
+            p = -4
+        elif param == 3:
+            p = -5
+        else:
+            print("wrong parameter for mode")
+            exit(1)
+        return int(str(self.codes[self.indicator])[p]) if len(str(self.codes[self.indicator])) > param + 1 else 0
+
+    def get_value(self, param):
+        mode = self.parameter_mode(param)
+        if mode == 0:  # position mode
+            index = self.codes[self.indicator + param]
+        elif mode == 1:  # immediate mode
+            index = self.indicator + param
+        elif mode == 2:  # relative mode
+            index = self.codes[self.indicator + param] + self.relative_base
+        else:
+            print("mode " + str(mode) + " not supported")
+            exit(1)
+        if index >= len(self.codes):
+            return 0
+        return self.codes[index]
+    
+    def add_value(self, position, value):
+        if position >= len(self.codes):
+            for _ in range(len(self.codes), position + 1):
+                self.codes.append(0)
+        self.codes[position] = value
+    
     def run(self):
         while self.indicator < len(self.codes):
             opcode = str(self.codes[self.indicator])[-2:].zfill(2)
-            print("opcodes " + opcode)
             if opcode == '99':
-                # print("reached 99 at " + str(self.indicator) + "\n")
+                #print("reached 99 at " + str(self.indicator) + "\n")
                 return self.output
             elif opcode == '01':
-                add_value(self.codes, self.codes[get_value(self.codes, parameter_mode(self.codes[self.indicator], 3), self.indicator, 3, self.relative_base)], get_value(self.codes, parameter_mode(self.codes[self.indicator], 1), self.indicator, 1, self.relative_base) + get_value(self.codes, parameter_mode(self.codes[self.indicator], 2), self.indicator, 2, self.relative_base))
+                self.add_value(self.codes[self.indicator + 3], self.get_value(1) + self.get_value(2))
                 self.indicator += 4
             elif opcode == '02':
-                add_value(self.codes, self.codes[get_value(self.codes, parameter_mode(self.codes[self.indicator], 3), self.indicator, 3, self.relative_base)], get_value(self.codes, parameter_mode(self.codes[self.indicator], 1), self.indicator, 1, self.relative_base) * get_value(self.codes, parameter_mode(self.codes[self.indicator], 2), self.indicator, 2, self.relative_base))
+                self.add_value(self.codes[self.indicator + 3], self.get_value(1) * self.get_value(2))
                 self.indicator += 4
             elif opcode == '03':
-                add_value(self.codes, self.codes[get_value(self.codes, parameter_mode(self.codes[self.indicator], 1), self.indicator, 1, self.relative_base)], self.input_queue.get())
+                self.codes[self.codes[self.indicator + 1]] = self.input_queue.get()
                 self.indicator += 2
             elif opcode == '04':
-                self.output = get_value(self.codes, parameter_mode(self.codes[self.indicator], 1), self.indicator, 1, self.relative_base)
+                self.output = self.get_value(1)
                 self.output_queue.put(int(self.output))
                 self.indicator += 2
             elif opcode == '05':
-                if get_value(self.codes, parameter_mode(self.codes[self.indicator], 1), self.indicator, 1, self.relative_base) > 0:
-                    self.indicator = get_value(self.codes, parameter_mode(self.codes[self.indicator], 2), self.indicator, 2, self.relative_base)
+                if self.get_value(1) > 0:
+                    self.indicator = self.get_value(2)
                 else:
                     self.indicator += 3
             elif opcode == '06':
-                if get_value(self.codes, parameter_mode(self.codes[self.indicator], 1), self.indicator, 1, self.relative_base) == 0:
-                    self.indicator = get_value(self.codes, parameter_mode(self.codes[self.indicator], 2), self.indicator, 2, self.relative_base)
+                if self.get_value(1) == 0:
+                    self.indicator = self.get_value(2)
                 else:
                     self.indicator += 3
             elif opcode == '07':
-                if get_value(self.codes, parameter_mode(self.codes[self.indicator], 1), self.indicator, 1, self.relative_base) < get_value(self.codes, parameter_mode(self.codes[self.indicator], 2), self.indicator, 2, self.relative_base):
-                    add_value(self.codes, self.codes[get_value(self.codes, parameter_mode(self.codes[self.indicator], 3), self.indicator, 3, self.relative_base)], 1)
+                if self.get_value(1) < self.get_value(2):
+                    self.add_value(self.codes[self.indicator + 3], 1)
                 else:
-                    add_value(self.codes, self.codes[get_value(self.codes, parameter_mode(self.codes[self.indicator], 3), self.indicator, 3, self.relative_base)], 0)
+                    self.add_value(self.codes[self.indicator + 3], 0)
                 self.indicator += 4
             elif opcode == '08':
-                if get_value(self.codes, parameter_mode(self.codes[self.indicator], 1), self.indicator, 1, self.relative_base) == get_value(
-                        self.codes, parameter_mode(self.codes[self.indicator], 2), self.indicator, 2, self.relative_base):
-                    add_value(self.codes, self.codes[get_value(self.codes, parameter_mode(self.codes[self.indicator], 3), self.indicator, 3, self.relative_base)], 1)
+                if self.get_value(1) == self.get_value(2):
+                    self.add_value(self.codes[self.indicator + 3], 1)
                 else:
-                    add_value(self.codes, self.codes[get_value(self.codes, parameter_mode(self.codes[self.indicator], 3), self.indicator, 3, self.relative_base)], 0)
+                    self.add_value(self.codes[self.indicator + 3], 0)
                 self.indicator += 4
             elif opcode == '09':
-                self.relative_base += get_value(self.codes, parameter_mode(self.codes[self.indicator], 1), self.indicator, 1, self.relative_base)
+                self.relative_base += self.get_value(1)
                 self.indicator += 2
             else:
                 print("should not happen at " + str(self.indicator) + "\n")
