c51b1c53326501c3455ac5bb43596e9416d6889e
qeqar <mark.eisenblaetter@gmail.com>
2020-12-01 06:53:25 +0100

    2020 day1 1 and 2

---
+++ 2020/day91.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+with open('inputs/day01.txt') as f:
+    lines = f.readlines()
+
+for first in lines:
+    first.strip()
+    for second in lines:
+        second.strip()
+        if first != second:
+            if int(first) + int(second) == 2020:
+                print(int(first) * int(second))
+
+for first in lines:
+    first.strip()
+    for second in lines:
+        second.strip()
+        for third in lines:
+            third.strip()
+            if first != second and first != third and second != third:
+                if int(first) + int(second) + int(third)== 2020:
+                    print(int(first) * int(second) * int(third))
+                    exit(0)
