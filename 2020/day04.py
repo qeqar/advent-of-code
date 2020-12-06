4055935e621e86db59b7bf9eb0715eccff37ea17
qeqar <mark.eisenblaetter@gmail.com>
2020-12-06 13:44:23 +0100

    regex fix

--- 2020/day04.py
+++ 2020/day04.py
@@ -6,6 +6,36 @@ with open('inputs/day04.txt') as f:
     lines = f.read().splitlines()
 
 
+def validate(ident_entry, ident_value):
+    if ident_entry == "byr":
+        if 1920 <= int(ident_value) <= 2002:
+            return True
+    elif ident_entry == "iyr":
+        if 2010 <= int(ident_value) <= 2020:
+            return True
+    elif ident_entry == "eyr":
+        if 2020 <= int(ident_value) <= 2030:
+            return True
+    elif ident_entry == "hgt":
+        if ident_value[-2:] == 'cm' and 150 <= int(ident_value[:-2]) <= 193:
+            return True
+        elif ident_value[-2:] == "in" and 59 <= int(ident_value[:-2]) <= 76:
+            return True
+    elif ident_entry == "hcl":
+        if re.match('^#[0-9a-f]{6}$', ident_value):
+            return True
+    elif ident_entry == "ecl":
+        if ident_value in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]:
+            return True
+    elif ident_entry == "pid":
+        if re.match('^[0-9]{9}$', ident_value):
+            return True
+    elif ident_entry == "cid":
+        return True
+
+    return False
+
+
 class Ident:
     byr = None
     iyr = None
@@ -19,35 +49,6 @@ class Ident:
     def show(self):
         print(self.byr, self.iyr, self.eyr, self.hgt, self.hcl, self.ecl, self.pid, self.cid)
 
-    def validate(self, ident_entry, ident_value):
-        if ident_entry == "byr":
-            if 1920 <= int(ident_value) <= 2002:
-                return True
-        elif ident_entry == "iyr":
-            if 2010 <= int(ident_value) <= 2020:
-                return True
-        elif ident_entry == "eyr":
-            if 2020 <= int(ident_value) <= 2030:
-                return True
-        elif ident_entry == "hgt":
-            if ident_value[-2:] == 'cm' and 150 <= int(ident_value[:-2]) <= 193:
-                return True
-            elif ident_value[-2:] == "in" and 59 <= int(ident_value[:-2]) <= 76:
-                return True
-        elif ident_entry == "hcl":
-            if re.match('#[0-9a-f]{6}', ident_value):
-                return True
-        elif ident_entry == "ecl":
-            if ident_value in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]:
-                return True
-        elif ident_entry == "pid":
-            if re.match('[0-9]{9}', ident_value):
-                return True
-        elif ident_entry == "cid":
-            return True
-
-        return False
-
 
 passports = []
 passport = Ident()
@@ -60,29 +61,53 @@ for line in lines:
         for entry in line.split(' '):
             ent, value = entry.split(':')
             if ent == "byr":
-                if passport.validate(ent, value):
-                    passport.byr = value
+                if validate(ent, value):
+                    if passport.byr is not None:
+                        exit(1)
+                    else:
+                        passport.byr = value
             elif ent == "iyr":
-                if passport.validate(ent, value):
-                    passport.iyr = value
+                if validate(ent, value):
+                    if passport.iyr is not None:
+                        exit(1)
+                    else:
+                        passport.iyr = value
             elif ent == "eyr":
-                if passport.validate(ent, value):
-                    passport.eyr = value
+                if validate(ent, value):
+                    if passport.eyr is not None:
+                        exit(1)
+                    else:
+                        passport.eyr = value
             elif ent == "hgt":
-                if passport.validate(ent, value):
-                    passport.hgt = value
+                if validate(ent, value):
+                    if passport.hgt is not None:
+                        exit(1)
+                    else:
+                        passport.hgt = value
             elif ent == "hcl":
-                if passport.validate(ent, value):
-                    passport.hcl = value
+                if validate(ent, value):
+                    if passport.hcl is not None:
+                        exit(1)
+                    else:
+                        passport.hcl = value
             elif ent == "ecl":
-                if passport.validate(ent, value):
-                    passport.ecl = value
+                if validate(ent, value):
+                    if passport.ecl is not None:
+                        exit(1)
+                    else:
+                        passport.ecl = value
             elif ent == "pid":
-                if passport.validate(ent, value):
-                    passport.pid = value
+                if validate(ent, value):
+                    if passport.pid is not None:
+                        exit(1)
+                    else:
+                        passport.pid = value
             elif ent == "cid":
-                if passport.validate(ent, value):
-                    passport.cid = value
+                if validate(ent, value):
+                    if passport.cid is not None:
+                        exit(1)
+                    else:
+                        passport.cid = value
             else:
                 print("NOPE should not happened: " + str(ent))
 
