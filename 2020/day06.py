395f456ee1bd262029380449bd8aada5f8558617
qeqar <mark.eisenblaetter@gmail.com>
2020-12-06 15:35:52 +0100

    2020 day06

---
+++ 2020/day06.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import collections
+
+with open('inputs/day06.txt') as f:
+    lines = f.read().splitlines()
+
+yes_count = 0
+yes_count2 = 0
+group = ""
+group_count = 0
+for line in lines:
+    if line == "":
+        yes_count += len(set(group))
+        group_result = collections.Counter(group)
+        for r in group_result.values():
+            print(r)
+            if r == group_count:
+                yes_count2 += 1
+        group = ""
+        group_count = 0
+    else:
+        group += line
+        group_count += 1
+
+print(yes_count)
+print(yes_count2)
